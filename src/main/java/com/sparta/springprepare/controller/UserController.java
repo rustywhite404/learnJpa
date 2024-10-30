1c0e0f4bd3006450cc396573288d792582f80c5a
rustywhite404 <rustywhite404@gmail.com>
2024-10-30 23:34:01 +0900

    회원가입 구현

---
+++ src/main/java/com/sparta/springprepare/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.sparta.springprepare.controller;
+import com.sparta.springprepare.dto.SignupRequestDto;
+import com.sparta.springprepare.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(@ModelAttribute SignupRequestDto signupRequestDto){
+        userService.signup(signupRequestDto);
+        return "redirect:/api/user/login-page";
+    }
+}
