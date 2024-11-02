ae61f94da559b6034baf4b93c7df81293c186969
rustywhite404 <rustywhite404@gmail.com>
2024-11-02 21:57:48 +0900

    validation 검증

--- src/main/java/com/sparta/springprepare/controller/ProductController.java
+++ src/main/java/com/sparta/springprepare/controller/ProductController.java
@@ -1,14 +1,15 @@
 package com.sparta.springprepare.controller;
 
+import com.sparta.springprepare.dto.ProductRequestDto;
 import com.sparta.springprepare.entity.User;
 import com.sparta.springprepare.entity.UserRoleEnum;
 import com.sparta.springprepare.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api")
@@ -33,5 +34,11 @@ public class ProductController {
         }
 
         return "redirect:/";
+    }
+
+    @PostMapping("/validation")
+    @ResponseBody
+    public ProductRequestDto testValid(@RequestBody @Valid ProductRequestDto requestDto) {
+        return requestDto;
     }
 }
