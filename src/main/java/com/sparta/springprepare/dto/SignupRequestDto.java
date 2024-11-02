4763d95d9cb501bd76216b6a0b7d34a97ae10458
rustywhite404 <rustywhite404@gmail.com>
2024-11-02 22:33:52 +0900

    email 정규식 validation

--- src/main/java/com/sparta/springprepare/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/springprepare/dto/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.sparta.springprepare.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +15,7 @@ public class SignupRequestDto {
     private String password;
     @Email
     @NotBlank
+    @Pattern(regexp = "^[\\w!#$%&'*+/=?`{|}~^.-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$",message = "이메일 형식이 올바르지 않습니다.")
     private String email;
     private boolean admin = false;
     private String adminToken = "";
